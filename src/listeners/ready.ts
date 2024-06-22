0ceabc61e4eb3cf0c5e107804ce31997d6527e5a
lambda-vaatu <juanmena1605@gmail.com>
2024-06-22 14:27:53 -0500

    refactor: change bot presence

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,6 +1,9 @@
-import { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 
 export default function readyListeneroid(e: Client<true>) {
   console.log(`✅ Bot connected as ${e.user.tag}`);
-  e.user.presence.set({ status: "idle", activities: [{ name: "Waiting..." }] });
+  e.user.presence.set({
+    status: "idle",
+    activities: [{ name: "your messages", type: ActivityType.Listening }],
+  });
 }
